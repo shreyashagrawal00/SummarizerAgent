e5db26dfc3af734e917c644d57ff6fafdd0c4d30
Shreyash Agrawal <shreyash.agrawal3004@gmail.com>
2026-03-02 00:18:02 +0530

    feat: add helmet middleware for enhanced security

--- server/app.js
+++ server/app.js
@@ -1,10 +1,12 @@
 import express from "express";
 import cors from "cors";
-
 import authRoutes from "./routes/authRoutes.js";
 import newsRoutes from "./routes/newsRoutes.js";
+import helmet from "helmet";
 
 const app = express();
+
+app.use(helmet());
 
 app.use(cors());
 app.use(express.json());
